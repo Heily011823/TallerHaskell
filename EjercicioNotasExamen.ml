2a039e4086933d0d6ff959a88c9f4b72810f73db
Maria Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2026-03-17 09:07:53 -0500

    Se hicieron cambios en el código

--- EjercicioNotasExamen.ml
+++ EjercicioNotasExamen.ml
@@ -1,23 +1,25 @@
-(* Ejercicio 3*)
+(* Ejercicio 3: Diseña un programa que modele el resultado de un examen de un estudiante. *)
 
 type nombre = string
 type nota = int
 
-type resultado = { nombre: nombre; nota: nota }
+type resultado = Resultado of nombre * nota
 
 let evaluar resultado =
-  if resultado.nota >= 60 then
-    "El estudiante " ^ resultado.nombre ^ " aprobó con una nota " ^ string_of_int resultado.nota
-  else
-    "El estudiante " ^ resultado.nombre ^ " reprobó con una nota " ^ string_of_int resultado.nota
+  match resultado with
+  | Resultado (nombre, nota) ->
+      if nota >= 60 then
+        Printf.sprintf "El estudiante %s aprobó con una nota %d" nombre nota
+      else
+        Printf.sprintf "El estudiante %s reprobó con una nota %d" nombre nota
 
 
 let () =
-  let resultado1 = { nombre = "Maria"; nota = 90 } in
+  let resultado1 = Resultado ("Maria", 90) in
   print_endline (evaluar resultado1);
 
-  let resultado2 = { nombre = "Andres"; nota = 54 } in
+  let resultado2 = Resultado ("Andres", 54)  in
   print_endline (evaluar resultado2);
 
-  let resultado3 = { nombre = "Paula"; nota = 78 } in
+  let resultado3 = Resultado ("Paula", 78) in
   print_endline (evaluar resultado3)
